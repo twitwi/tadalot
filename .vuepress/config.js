0677f0b4107fb7fdb192c3aa07140e65e6564b5f
Rémi Emonet <remi.emonet@reverse--com.heeere>
2018-06-28 10:30:21 +0200

    Fix gitignore and missing config

---
+++ .vuepress/config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    title: 'TADALoT',
+    description: 'Tadalot: Anomaly Detection by Machine Learning using Atypical Losses and Transfer-learning',
+    base: '/tadalot/',
+}
